7c734e6b1b7a53c5a32a9e31d99c45d38996a24f
Indithem <128889127+Indithem@users.noreply.github.com>
2023-10-10 21:37:56 +0530

    regular BST

--- src/eppcompiler.cpp
+++ src/eppcompiler.cpp
@@ -4,12 +4,13 @@
 
 //Write your code below this line
 
-EPPCompiler::EPPCompiler(){
+// EPPCompiler::EPPCompiler(){
 
-}
+// }
 
 EPPCompiler::EPPCompiler(string out_file,int mem_limit){
-
+    output_file=out_file;
+    memory_size=mem_limit;
 }
 
 void EPPCompiler::compile(vector<vector<string>> code){
@@ -17,11 +18,16 @@ void EPPCompiler::compile(vector<vector<string>> code){
 }
 
 vector<string> EPPCompiler::generate_targ_commands(){
-
+    ExprTreeNode* root= targ.expr_trees.back();
+    vector<string> commands;
+    
 }
 
 void EPPCompiler::write_to_file(vector<string> commands){
-
+    fstream writer;
+    writer.open(output_file,ios::app);
+    for (string& command:commands){writer<<command<<"\n";}
+    // writer.close(); destructor should call it!
 }
 
 EPPCompiler::~EPPCompiler(){
