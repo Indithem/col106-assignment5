2ebd13afefafc7bf8f729ae60b5b89d03dfee957
Indithem <128889127+Indithem@users.noreply.github.com>
2023-10-10 22:17:29 +0530

    made compiler, need to make targ command

--- src/eppcompiler.cpp
+++ src/eppcompiler.cpp
@@ -14,12 +14,41 @@ EPPCompiler::EPPCompiler(string out_file,int mem_limit){
 }
 
 void EPPCompiler::compile(vector<vector<string>> code){
+    {fstream writer;
+    writer.open(output_file,ios::trunc);
+    writer.close();}
 
+    least_mem_loc.push_heap(-memory_size);
+    SymbolTable* symtable = targ.symtable;
+
+    string varID;int del_loc;
+    for (vector<string>& commands:code){
+        targ.parse(commands);
+
+        if (targ.expr_trees.back()->left->type=="VAR"){
+            varID=targ.expr_trees.back()->left->id;
+            if (symtable->search(varID)==-2){
+                symtable->insert(varID);
+                symtable->assign_address(varID,least_mem_loc.get_min());least_mem_loc.pop();
+            }
+        }
+
+        write_to_file(generate_targ_commands());
+        if (targ.expr_trees.back()->type=="DEL"){
+            varID=targ.expr_trees.back()->right->id;
+            del_loc=symtable->search(varID);
+            if(del_loc>=0){least_mem_loc.push_heap(del_loc);}
+            symtable->remove(varID);
+        }
+    }
 }
 
 vector<string> EPPCompiler::generate_targ_commands(){
+    // we had given memory to the new variable
     ExprTreeNode* root= targ.expr_trees.back();
+    SymbolTable* symtable = targ.symtable;
     vector<string> commands;
+
     
 }
 
