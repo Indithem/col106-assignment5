8f0eabe7ca938e12306325a0ecd2b87fc92f9100
Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2023-10-11 10:43:35 +0530

    Justa bst (#1)
    * regular BST
    
    * made compiler, need to make targ command
    
    * hopefully done with regular BST
    
    * made a working launch.json for vscode
    
    * fixed minheap bubble_to_down
    
    * .vscode
    
    * Minor bug fixe to deleting in parser vector

--- src/eppcompiler.cpp
+++ src/eppcompiler.cpp
@@ -4,26 +4,125 @@
 
 //Write your code below this line
 
-EPPCompiler::EPPCompiler(){
+// EPPCompiler::EPPCompiler(){
 
-}
+// }
 
 EPPCompiler::EPPCompiler(string out_file,int mem_limit){
-
+    output_file=out_file;
+    memory_size=mem_limit;
+    least_mem_loc.push_heap(-mem_limit);
 }
 
 void EPPCompiler::compile(vector<vector<string>> code){
+    {ofstream writer;
+    writer.open(output_file,ios::trunc);
+    writer<<"\n";writer.close();}
 
+    SymbolTable* symtable = targ.symtable;
+
+    string varID;int del_loc;
+    for (vector<string>& commands:code){
+        targ.parse(commands);
+
+        if (targ.expr_trees.back()->left->type=="VAR"){
+            varID=targ.expr_trees.back()->left->id;
+            if (symtable->search(varID)==-2){
+                symtable->insert(varID);
+                symtable->assign_address(varID,least_mem_loc.get_min());least_mem_loc.pop();
+            }
+        }
+
+        write_to_file(generate_targ_commands());
+        if (targ.expr_trees.back()->left->type=="DEL"){
+            varID=targ.expr_trees.back()->right->id;
+            del_loc=symtable->search(varID);
+            if(del_loc>=0){least_mem_loc.push_heap(del_loc);}
+            symtable->remove(varID);
+        }
+        delete targ.expr_trees.back();
+        targ.expr_trees.pop_back();
+    }
 }
 
+struct VisitNode
+{
+    ExprTreeNode* node;
+    // short unsigned visits=0;
+    bool visited=false;
+};
+
+
 vector<string> EPPCompiler::generate_targ_commands(){
+    // we had given memory to the new variable
+    ExprTreeNode* root= targ.expr_trees.back();
+    SymbolTable* symtable = targ.symtable;
+    vector<string> commands;commands.reserve(5);
+    
+
+    string line;
+    if (root->left->type=="DEL"){
+        line="DEL = mem[";
+        line=line+to_string(symtable->search(root->right->id))+']';
+        commands.push_back(line);return commands;
+    }
+
+    vector<VisitNode> stack;
+    stack.push_back(VisitNode{root->right,false});
+    VisitNode* current;ExprTreeNode*l,*r;
+    while(not stack.empty()){
+        current=&stack.back();
+        // if(current->visits==2){
+        if(current->visited){
+            commands.push_back(current->node->type);
+            stack.pop_back();continue;
+        }
+        else{
+            if(current->node->type=="VAL"){
+                line="PUSH ";
+                line=line+to_string(current->node->num);
+                commands.push_back(line);stack.pop_back();continue;
+            }
+            if(current->node->type=="VAR"){
+                line="PUSH mem[";
+                line=line+to_string(symtable->search(current->node->id))+']';
+                commands.push_back(line);stack.pop_back();continue;
+            }
+            // current->visits+=1;
+            current->visited=true;
+            l=current->node->left;
+            r=current->node->right;
+            stack.push_back(VisitNode{l,false});
+            stack.push_back(VisitNode{r,false});
+        }
+    }
+
+    if(root->left->type=="RET"){
+        commands.push_back("RET = POP");
+        return commands;
+    }
+
+    line="mem[";
+    line=line+to_string(symtable->search(root->left->id))+"] = POP";
+    commands.push_back(line);
+    return commands;
 
 }
 
 void EPPCompiler::write_to_file(vector<string> commands){
-
+    ofstream writer;
+    writer.open(output_file,ios::app);
+    for (string& command:commands){writer<<command<<"\n";}
+    // writer.close(); destructor should call it!
 }
 
 EPPCompiler::~EPPCompiler(){
   
 }
+
+// #include<iostream>
+// int main(){
+//     EPPCompiler c{"test1.txt",2};
+//     vector<vector<string>> comands={{"x",":=","(","5","-","2",")"},{"ret",":=","x"}};
+//     c.compile(comands);
+// }
--- src/symtable.cpp
+++ src/symtable.cpp
@@ -9,25 +9,84 @@ SymbolTable::SymbolTable(){
 }
 
 void SymbolTable::insert(string k){
-    size+=1;
-    if(root==NULL){root= new SymNode{k};return;}
-    SymNode* current= root;
-    while(current!=NULL){
-        if(current->key>k){current=current->left;continue;}
-        current=current->right;
+     size++;
+    if(root==nullptr){root=new SymNode{k};return;}
+
+    SymNode*prev_position{root},*position=root;
+    while(position!=nullptr){
+        prev_position=position;
+        if (position->key>k){position=position->left;}
+        else{position=position->right;}
     }
-
-    current=current->par;
-    SymNode* created=new SymNode{k};
-    created->par=current;
-    if(current->key>k){current->left=created;current->height-=1;} else{current->right=created;current->height+=1;}
-
+    if(prev_position->key>k){prev_position->left=\
+    new SymNode{k};return;}
+    else{prev_position->right= new SymNode{k};return;}
     //...need to complete...
 }
 
 void SymbolTable::remove(string k){
-    if(size==0){return;}
-    size-=1;
+    SymNode*node=root;
+    SymNode*parent=root;
+    while(node!=nullptr and node->key!=k){
+        parent=node;
+        if (node->key>k){node=node->left;}
+        else{node=node->right;}
+    }
+
+    if(node==nullptr){return;}
+    int checking = (node->left==nullptr) +2*(node->right==nullptr);
+    bool node_on_left = (parent->left==node);
+
+    switch (checking)
+    {
+    case 3:
+        size--;
+        if(node==root){root=nullptr;}
+        delete node;
+        return;
+    case 2:
+        if(node_on_left){parent->left=node->left;}
+        else{parent->right=node->left;}
+        if(node==root){root=node->right;}
+        node->left=nullptr;node->right=nullptr;
+        delete node;
+        size--;
+    return;
+    case 1:
+        if(node_on_left){parent->left=node->right;}
+        else{parent->right=node->right;}
+        if(node==root){root=node->right;}
+        node->right=nullptr;node->left=nullptr;
+        delete node;
+        size--;
+    return;
+    case 0:
+        SymNode* replacable,*replacable_parent;
+        
+        replacable_parent=node;
+        replacable=node->right;
+
+        while(replacable->left!=nullptr){replacable_parent=replacable;replacable=replacable->left;}
+
+        if(node==root){root=replacable;}
+
+        if(node_on_left){parent->left=replacable;}
+        else{if(node!=root){parent->right=replacable;}}
+
+        replacable->left=node->left;
+
+        if(replacable_parent!=node){
+            replacable_parent->left=replacable->right;
+            replacable->right=node->right;
+        }
+
+        node->left=nullptr;node->right=nullptr;
+        delete node;
+        size--;
+        return;
+
+    return;
+    }
 }
 
 int SymbolTable::search(string k){
